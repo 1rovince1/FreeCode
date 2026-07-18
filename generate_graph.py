6e353861174faac4c3d55e62bf9428038ca6e12e
Rovince Gangwar <rovince2803@gmail.com>
2026-07-18 19:54:14 +0530

    added ci-cd

---
+++ generate_graph.py
@@ -0,0 +1,7 @@
+from harnesses.coding_harness.orchestration import compiled_harness
+
+
+try:
+    compiled_harness.get_graph(xray=True).draw_png(output_file_path="graph.png")
+except Exception as e:
+    print(f"Error generating graph.png: {e}")
